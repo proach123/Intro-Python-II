80fe0a989c24822b7a356a9befe453db8f9ffe3e
Patrick Roach <patrickevanroach@gmail.com>
2020-05-07 18:58:14 -0600

    fixed the commit now

--- src/room.py
+++ src/room.py
@@ -1,2 +1,18 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, subtext, items):
+        self.name = name
+        self.subtext = subtext
+        self.items = []
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def print_description(self):
+        return f"{self.subtext}"
+
+    def print_items(self):
+        return f"{self.items}"
+
